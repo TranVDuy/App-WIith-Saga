21c72b52def5da508b0fd778544f5b773c4882ec
Tran Van Duy <tranvanduy28032001@gmail.com>
2024-04-14 15:27:04 +0700

    Setup Connected React Router

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,18 +1,24 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './rootSaga';
 import authReducer from 'features/auth/authSlice';
+import { connectRouter } from 'connected-react-router';
+import { history } from 'utils';
+import { routerMiddleware } from 'connected-react-router';
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  auth: authReducer,
+  router: connectRouter(history),
+})
 
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    auth: authReducer
-  },
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) => 
-    getDefaultMiddleware().concat(sagaMiddleware),
+    getDefaultMiddleware().concat(sagaMiddleware, routerMiddleware(history)),
 });
 sagaMiddleware.run(rootSaga);
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,10 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import {CssBaseline} from '@mui/material';
+import { history } from 'utils';
+import { ConnectedRouter } from 'connected-react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -14,10 +16,12 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <CssBaseline/>
-        <App />
-      </BrowserRouter>
+        {/* <ConnectedRouter history={history}> */}
+        <BrowserRouter>
+          <CssBaseline/>
+          <App />
+        </BrowserRouter>
+        {/* </ConnectedRouter> */}
     </Provider>
   </React.StrictMode>
 );
